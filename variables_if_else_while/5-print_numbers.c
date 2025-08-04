f5e93a929342efdcc41a246f5a3bbb15aac74c55
quliyev223 <mikayilquliev@gmail.com>
2025-08-04 17:09:48 +0000

    task

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+
+	for  (c = '0'; c < '10'; c++)
+	putchar(c);	
+
+	putchar("\n")
+	return(0);
+}
