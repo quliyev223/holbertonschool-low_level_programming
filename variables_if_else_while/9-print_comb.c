67193c3d550521763eed37a01b9211af4e231f47
quliyev223 <mikayilquliev@gmail.com>
2025-08-05 13:58:55 +0000

    task

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits from 0 to 9 separated by ", "
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
+ 
