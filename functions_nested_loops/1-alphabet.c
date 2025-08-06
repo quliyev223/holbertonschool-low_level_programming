f8aa119e55ed2f4b6f9a9891b04ba9f5940fa9bf
quliyev223 <mikayilquliev@gmail.com>
2025-08-06 08:35:59 +0000

    task

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the lowercase alphabet followed by a newline
+ */
+void print_alphabet(void)
+{
+    char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
+    int i = 0;
+
+    while (alphabet[i] != '\0')
+    {
+        _putchar(alphabet[i]);
+        i++;
+    }
+}
