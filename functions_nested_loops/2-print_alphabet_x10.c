83ceb767de7cc9d9bc1ef26deca60433c1bfb19c
quliyev223 <mikayilquliev@gmail.com>
2025-08-07 06:40:36 +0000

    task

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main -  print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
  *
  * Return: 0 (success)
  */
