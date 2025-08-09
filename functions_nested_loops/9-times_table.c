c321963aa3cce3dd33f1a2e4850a233af40a391c
quliyev223 <mikayilquliev@gmail.com>
2025-08-09 08:59:38 +0000

    task

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,12 +9,12 @@ void times_table(void)
 {
 	int row, col, product;
 
-
 	for (row = 0; row <= 9; row++)
 	{
 		for (col = 0; col <= 9; col++)
 		{
 			product = row * col;
+
 			if (col == 0)
 			{
 				_putchar(product + '0');
@@ -23,18 +23,18 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				
+
 				if (product < 10)
 				{
 					_putchar(' ');
 					_putchar(product + '0');
-				}	
+				}
 				else
 				{
 					_putchar((product / 10) + '0');
 					_putchar((product % 10) + '0');
 				}
-			}	
+			}
 		}
 		_putchar('\n');
 	}
