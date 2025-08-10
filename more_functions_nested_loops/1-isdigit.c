7cc3bcab2b1b1ba259d56eaee5b63ea65f16501f
quliyev223 <mikayilquliev@gmail.com>
2025-08-10 08:16:14 +0000

    task

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -2,14 +2,14 @@
 
 /**
  * _isdigit -  checks for a digit (0 through 9).
- * @c - is a digit 
- * 
+ * @c - is a digit
+ *
  * Return: 1 if c is a digit
- * 		0 otherwise
+ * 	0 otherwise
  */
 int _isdigit(int c)
 {
-	if (c >= '0' && c<='9')
+	if (c >= '0' && c <='9')
 		return (1);
 	else
 		return (0);
