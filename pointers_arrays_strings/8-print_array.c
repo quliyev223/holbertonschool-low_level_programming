61ff8de9bc6d9f483cfc86c98b6bcd7a5ccdcfe3
quliyev223 <mikayilquliev@gmail.com>
2025-08-13 14:37:09 +0000

    task

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integeres 
+ * @a: pointer to the array of integers
+ * @n: number of elements to print
+ *
+ * Description: This function prints elements of an integer array
+ * separated by a comma and a space, in the same order as they are 
+ * stored in the array. Ends with a new line.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n-1) /* Add comma and space except for the last element */
+		{
+			printf(", ");
+		}
+	}
+	printf("\n"); /* Print new line at the end */
+}
