c26450502b5afcfc6fd55a29586e6f749d00fa43
quliyev223 <mikayilquliev@gmail.com>
2025-08-14 12:30:09 +0000

    task

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - converts a string to an integer
@@ -8,6 +9,7 @@
  * sequence of digits found into an integer. It considers all the
  * '+' and '-' signs before the first digit, changing the sign
  * accordingly. If no digits are found, it returns 0.
+ * It prevents signed integer overflow by stopping at INT_MAX/INT_MIN.
  *
  * Return: the integer value, or 0 if no digits are found
  */
@@ -23,16 +25,27 @@ int _atoi(char *s)
 			sign *= -1;
 		else if (*s == '+')
 		{
-			/* skip the plus sign */
+			/* skip plus sign */
 		}
 		else if (*s >= '0' && *s <= '9')
 		{
+			int digit = *s - '0';
+
 			found_digit = 1;
-			result = (result * 10) + (*s - '0');
+
+			/* check overflow before multiplying by 10 and adding */
+			if (result > (INT_MAX - digit) / 10)
+			{
+				if (sign == 1)
+					return (INT_MAX);
+				else
+					return (INT_MIN);
+			}
+
+			result = (result * 10) + digit;
 		}
 		else if (found_digit)
 		{
-			/* stop when non-digit is found after number has started */
 			break;
 		}
 		s++;
