48ec98c84b9f74a3a86adf41ebcca1f26e0c3c3b
quliyev223 <mikayilquliev@gmail.com>
2025-08-14 09:29:12 +0000

    atsk

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -4,44 +4,32 @@
  * _atoi - converts a string to an integer
  * @s: the string to convert
  *
- * Return: the integer value of the string, or 0 if no numbers found
- *
- * Description: The function parses the string to find an integer.
- * It takes into account all preceding '+' and '-' signs
- * Conversion stops at the first non_digit character after the number.
+ * Return: the integer value, or 0 if no numbers
  */
 int _atoi(char *s)
 {
-	int sign = 1; /* store the sign of the number */
-	int result = 0; /* store the resulting integer */
-	int started = 0; /* flag to indicate if number parsing has started */
+    int sign = 1, result = 0, found_digit = 0;
 
+    while (*s)
+    {
+        if (*s == '-')
+        {
+            if (!found_digit)
+                sign = -sign;
+        }
+        else if (*s == '+')
+        {
+            /* do nothing for '+' before number */
+        }
+        else if (*s >= '0' && *s <= '9')
+        {
+            found_digit = 1;
+            result = result * 10 + (*s - '0');
+        }
+        else if (found_digit)
+            break; /* stop at first non-digit after number */
+        s++;
+    }
 
-	while (*s != '\0')
-	{
-		/* handle signs befor the number starts */
-		if (!started)
-		{
-			if (*s == '-')
-				sign = -sign;
-			else if (*s == '+')
-				; /* do nothing for '+' */
-			else if (*s >= '0' && *s <= '9')
-			{
-				started = 1;
-				result = result * 10 + (*s - '0');
-			}
-		}
-		else 
-		{
-			/* continue building the number */
-			if (*s >= '\0' && *s <= '9')
-				result = result * 10 + (*s - '0');
-			else
-				break; /* stop at first non-digit character */
-		}
-		s++;
-	}
-
-	return sign * result;
-}
+    return sign * result;
+}	
