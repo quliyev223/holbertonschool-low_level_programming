3a60a76ecdbf0921cf4c1caa722cda8d46ac50e4
quliyev223 <mikayilquliev@gmail.com>
2025-08-14 15:41:48 +0000

    task

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two string
+ * @dest: destination string
+ * @src: source string to append
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
