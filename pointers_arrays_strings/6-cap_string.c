594c0b60837a77105470df17dbc349251ea77ab2
quliyev223 <mikayilquliev@gmail.com>
2025-08-15 12:46:13 +0000

    task

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string
+ * @str: Pointer to the string to modify
+ *
+ * Return: Pointer to the modified string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+	char separators[] = "\t\n,;.!?\"90{}";
+
+
+	/* Capitalize the first character if it is lowercase */
+	if (str[i] >= 'a' && str[i] <= 'z')
+		str[i] = str[i] - 32;
+	i++;
+
+	while (str[i] != '\0')
+	{ 
+		int j = 0;
+
+
+		/* Check if previous character is a separator */
+		while (separators[j] != '\0')
+		{
+			if (str[i - 1] == separators[j])
+			{
+				/* Capitalize current character if lowercase */
+				if (str[i] >= 'a' && str[i] <= 'z')
+					str[i] = str[i] - 32;
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+
+
+	return (str);
+}
