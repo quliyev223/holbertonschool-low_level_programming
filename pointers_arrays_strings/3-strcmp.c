57ce9f932a996875d5b974e332f29adfa69d5d9a
quliyev223 <mikayilquliev@gmail.com>
2025-08-15 11:41:19 +0000

    task

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s1: Pointer to the first string
  * @s2: Pointer to the second string
  *
- * REturn: An integer less than, equal to, or greater than 0 if s1 is found,
+ * Return: An integer less than, equal to, or greater than 0 if s1 is found,
  * respectively, to be less than, to match, or be greater than s2
  */
 int _strcmp(char *s1, char *s2)
