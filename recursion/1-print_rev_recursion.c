4a49803101c19e1e5c17f223935b483d17d37a4e
quliyev223 <mikayilquliev@gmail.com>
2025-08-19 08:41:01 +0000

    task

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string top rint
+ *
+ * Description: This function prints the string s in reverse 
+ * using recursion. Each character is printed after the
+ * recursive call for the next character.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
