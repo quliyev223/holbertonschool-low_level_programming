c0e467ea0da1f3014cfbda863087d4054de3dfcf
quliyev223 <mikayilquliev@gmail.com>
2025-08-20 09:10:13 +0000

    task

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to the program
+ * @args: number of command line arguments
+ * @argv: array of command line argument strings (unused)
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char * argv[] __attribute__((unused)))
+{
+	printf("%c\n", argc);
+	return (0);
+}
