d897fc7901c0977c76fde3612a29f51228eee297
quliyev223 <mikayilquliev@gmail.com>
2025-08-21 09:06:56 +0000

    task

---
+++ argc_argv/2-args.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Argument count, represents the number oof arguments passed
+ * @argv: Argument vector, array of strings containing the arguments
+ *
+ * Description: This program prints all the arguments it receives,
+ * including the first one which is the program name.
+ * Each argument is printed on a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+
+	for(i = 0; i < argc - 1; i++)
+		printf("%s\n", argv[i]);
+
+
+	return (0);
+}
+
+	
+
