aa9d03f3899369dda8d1d25c9d24179d6161f8a8
quliyev223 <mikayilquliev@gmail.com>
2025-08-24 08:13:16 +0000

    task

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1, len2 = 0, i,j;
+	unsigned int len1 = 0, len2 = 0, i,j;
 	char *new_str;
 
 
@@ -44,15 +44,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (j = 0; j < n; j++)
 		new_str[i + j] = s2[j];
 
-
 	new_str[i + j] = '\0';
-
 
 	return (new_str);
 }
-
-	
-		
-
-		
-
