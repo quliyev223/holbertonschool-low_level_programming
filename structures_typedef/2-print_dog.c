c65a1c7c075e9bdd782abcffd418d859539ca468
quliyev223 <mikayilquliev@gmail.com>
2025-08-29 12:17:14 +0000

    task

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	
+
 
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
@@ -28,7 +28,7 @@ void print_dog(struct dog *d)
 
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
-	
+
 
 	else
 		printf("Owner: %s\n", d->owner);
