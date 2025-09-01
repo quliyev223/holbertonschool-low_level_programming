ec6d76b7b40c3667a8b169ff16c41e431fdaaa83
quliyev223 <mikayilquliev@gmail.com>
2025-09-01 07:32:27 +0000

    task

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,19 +1,16 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-
 
 /**
  * print_name - calls a function to print a name
  * @name: the name to be printed
  * @f: pointer to a function that takes a string and returns nothing
  *
- * Description: This function checks if the given string 
+ * Description: This function checks if the given string
  * and function pointer are not NULL. If they are valid,
- * it calls the function pointed to by @f, passing @nmae as its argument.
- * This allows priinting the name in different styles depending
+ * it calls the function pointed to by @f, passing @name as its argument.
+ * This allows printing the name in different styles depending
  * on the function passed.
  *
  * Return: Nothing.
@@ -23,4 +20,3 @@ void print_name(char *name, void (*f)(char *))
 	if (name != NULL && f != NULL)
 		f(name);
 }
-#endif
