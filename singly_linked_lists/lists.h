62cb311ae765cd684e756cfe343c3109234e06d6
quliyev223 <mikayilquliev@gmail.com>
2025-09-08 12:16:27 +0000

    task

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -13,8 +13,8 @@
 typedef struct list_s
 {
 	char *str;
-    	unsigned int len;
-    	struct list_s *next;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
