d7a5e1144740ae3d8f3d0a63d7c9dec4a6475b5d
quliyev223 <mikayilquliev@gmail.com>
2025-09-10 07:53:37 +0000

    task

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -22,7 +22,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 
-	new_node->str == strdup(str);
+	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
