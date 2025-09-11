5b8118dc14158877038ca1472f0f02eaa1582e9a
quliyev223 <mikayilquliev@gmail.com>
2025-09-11 07:42:33 +0000

    task

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define LISTS_H
 
 #include <stddef.h>
+#include <stdio.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -21,5 +22,23 @@ typedef struct dlistint_s
 size_t print_dlistint(const dlistint_t *h);
 
 size_t dlistint_len(const dlistint_t *h);
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * 
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
 #endif
