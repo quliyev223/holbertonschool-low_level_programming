a389dce47828e1ce273435973ef493ac21600cd1
quliyev223 <mikayilquliev@gmail.com>
2025-09-11 08:46:46 +0000

    task

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -27,13 +27,13 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		*head = new_node;
 		return (new_node);
 	}
-	
+
 	temp = *head;
 	while (temp->next != NULL)
 		temp = temp->next;
-	
+
 	temp->next = new_node;
 	new_node->prev = temp;
-	
+
 	return (new_node);
 }
