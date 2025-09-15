d40524a07bcf24b657a2e0c08de6197b4c66a4c5
quliyev223 <mikayilquliev@gmail.com>
2025-09-15 17:12:49 +0000

    task

---
+++ file_io/1-create_file.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file and writes text into it
+ *
+ * @filename: name of the file to create
+ * @text_content: string to write, can be NULL
+ *
+ * REturn: 1 on success, -1 on failure
+ *
+ * Description: Checks filename. Opens or creates file.
+ * Sets permissions rw- - - - - - - -.
+ * Writes text_content if not NULL. Closes file. Returns 1 on success.
