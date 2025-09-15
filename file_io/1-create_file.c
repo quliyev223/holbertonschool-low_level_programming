74f652ef698cd1c03d33b255c86a65f78c78ce94
quliyev223 <mikayilquliev@gmail.com>
2025-09-15 17:30:57 +0000

    task

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,3 +11,12 @@
  * Description: Checks filename. Opens or creates file.
  * Sets permissions rw- - - - - - - -.
  * Writes text_content if not NULL. Closes file. Returns 1 on success.
+ */
+int create_file(const char *filename, char *text_content);
+{
+	int fd;
+
+
+	if (filename == NULL)
+		return (-1);
+
