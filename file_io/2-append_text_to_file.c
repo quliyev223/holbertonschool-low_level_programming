fcaa615788659411e689796aad9bf5bea252274f
quliyev223 <mikayilquliev@gmail.com>
2025-09-17 06:31:49 +0000

    task

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -4,10 +4,10 @@
  * append_text_to_file - Appends text to an existing file.
  * @filename: Name of the file.
  * @text_content: NULL-terminated string to add at the end.
- * 
+ *
  * Description: Does not create the file if it doesn't exist.
  * If text_content is NULL, does nothing and returns 1 if file exists.
- * 
+ *
  * Return: 1 on success, -1 on failure (file doesn't exist, no permission
  * or error).
  */
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	
+
 	if (text_content == NULL)
 	{
 		close(fd);
