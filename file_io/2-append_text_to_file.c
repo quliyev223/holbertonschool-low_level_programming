66ba0f40fb14ca17a3e70dcfee28de2b29f49685
quliyev223 <mikayilquliev@gmail.com>
2025-09-17 06:28:11 +0000

    task

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -25,8 +25,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	
 	if (text_content == NULL)
+	{
+		close(fd);
 		return (1);
-
+	}
 	while (text_content[length] != '\0')
 		length++;
 
