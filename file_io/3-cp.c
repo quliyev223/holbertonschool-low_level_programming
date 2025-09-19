a0578a029bf7e4ce64fa48d1c87cba422bd33286
quliyev223 <mikayilquliev@gmail.com>
2025-09-19 05:41:00 +0000

    task

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - copies content of one file to another
  * @argc: number of arguments
@@ -9,25 +10,57 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd1from;
-	int fd2to;
+	int fd_from;
+	int fd_to;
 	char buffer[1024];
+	ssize_t n_read, n_written ;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n", argv[1]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd1from = open(argv[1], O_RDONLY);
-	if (fd1from = -1)
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE %s\n", argv[1]);
 		exit(98);
 	}
-	fd2to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd2to = -1)
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE"\n);
+		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE %s\n", argv[2]);
 		exit(99);
 	}
-	
+	while ((n_read = read(fd_from, buffer, 1024)) > 0)
+	{
+		n_written = write(fd_to, buffer, n_read);
+		if (n_written != n_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
+		}
+	}
+
+	if (n_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+
+	return (0);
+}
