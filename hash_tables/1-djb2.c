7d445ace82b36caa67c58d95bea059aa5064e913
quliyev223 <mikayilquliev@gmail.com>
2025-09-20 12:44:42 +0000

    task

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Implements the djb2 hashing algorithm.
+ * @str: The string to hash.
+ *
+ * Return: The computed hash as an unsigned long int.
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+    unsigned long int hash = 5381;
+    int c;
+
+    while ((c = *str++))
+    {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+    return (hash);
+}
