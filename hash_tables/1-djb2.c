baf86b233bd2aafe50a910872c96f882723f5485
quliyev223 <mikayilquliev@gmail.com>
2025-09-20 12:53:12 +0000

    task

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -12,8 +12,8 @@ unsigned long int hash_djb2(const unsigned char *str)
 	int c;
 
 	while ((c = *str++))
-        {
+	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-        }
+	{
 	return (hash);
 }
