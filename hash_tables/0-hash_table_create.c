82091b0f241b917db3f2f2ff1e7afa5bbe7cdb29
quliyev223 <mikayilquliev@gmail.com>
2025-09-20 12:25:46 +0000

    task

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -28,7 +28,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	for(i = 0; i < size; i++)
-	array[i] = NULL;
+	hashtable->array[i] = NULL;
 
 	hashtable->size = size;
 	hashtable->array = array;
