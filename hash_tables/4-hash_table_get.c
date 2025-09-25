42ef3d0423c146a74073ca96293fea3857e18d95
quliyev223 <mikayilquliev@gmail.com>
2025-09-25 07:03:21 +0000

    task

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -17,7 +17,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	if (ht == NULL || key == NULL || key[0] == '\0')
 	       return (NULL);
-	
+
 	index = key_index((const unsigned char *)key, ht->size);
 	head = ht->array[index];
 
